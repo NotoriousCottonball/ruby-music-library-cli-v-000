2a0689c855bcab64fddff5e0ea9297c50e5a3bf7
George  Pianka <gpianka1@gmail.com>
2018-08-03 10:16:13 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -2,7 +2,7 @@ class MusicLibraryController
   attr_accessor :path, :songs
    @@song_list = []
    def initialize(path = "./db/mp3s")
-    MusicImporter.new(path).
+    MusicImporter.new(path).tap{|i| i.
     importer.import
     @song_list = []
   end
